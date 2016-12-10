70207a56c59d49a4ad9caf0167a9126dab2b5655
Carlos Cordoba <ccordoba12@gmail.com>
2016-12-10 11:43:11 -0500

    Diffusion: Add support for subplots in plot_adopters
    - Also add a fontsize parameter

--- Diffusion/plots.py
+++ Diffusion/plots.py
@@ -11,7 +11,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def plot_adopters(data, par_name, par_value, cumulative= False):
+def plot_adopters(data, par_name, par_value, axis=None, cumulative=False,
+                  fontsize=15):
     """
     data contains the output of compute_run
     """
@@ -19,8 +20,17 @@ def plot_adopters(data, par_name, par_value, cumulative= False):
         data[0] = map(np.cumsum, data[0])
         data[1] = map(np.cumsum, data[1])
 
-    sns.tsplot(data=data[0], condition='Without Reflexivity')
-    sns.tsplot(data=data[1], color='m', condition='With Reflexivity')
-    plt.title(r'$%s = %s$' % (par_name, str(par_value)), fontsize=15)
-    plt.legend(loc='upper right', fontsize=13)
-    plt.tick_params(axis='both', which='major', labelsize=13)
+    if axis:
+        sns.tsplot(data=data[0], condition='Without Reflexivity', ax=axis)
+        sns.tsplot(data=data[1], color='m', condition='With Reflexivity',
+                   ax=axis)
+        axis.set_title(r'$%s = %s$' % (par_name, str(par_value)),
+                       fontsize=fontsize)
+        axis.legend(loc='upper right', fontsize=fontsize-2)
+        axis.tick_params(axis='both', which='major', labelsize=fontsize-2)
+    else:
+        sns.tsplot(data=data[0], condition='Without Reflexivity')
+        sns.tsplot(data=data[1], color='m', condition='With Reflexivity')
+        plt.title(r'$%s = %s$' % (par_name, str(par_value)), fontsize=fontsize)
+        plt.legend(loc='upper right', fontsize=fontsize-2)
+        plt.tick_params(axis='both', which='major', labelsize=fontsize-2)
