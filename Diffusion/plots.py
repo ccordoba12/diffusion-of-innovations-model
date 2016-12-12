7584afc76d5865f8a98b3a0b9be94b4d4d302cd3
Carlos Cordoba <ccordoba12@gmail.com>
2016-12-11 22:01:56 -0500

    Diffusion: Change plot_adopters for changes in compute_run

--- Diffusion/plots.py
+++ Diffusion/plots.py
@@ -16,21 +16,24 @@ def plot_adopters(data, par_name, par_value, axis=None, cumulative=False,
     """
     data contains the output of compute_run
     """
+    no_rx_data = [d['adopters'] for d in data['no_rx']]
+    rx_data = [d['adopters'] for d in data['rx']]
+
     if cumulative:
-        data[0] = map(np.cumsum, data[0])
-        data[1] = map(np.cumsum, data[1])
+        no_rx_data = map(np.cumsum, no_rx_data)
+        rx_data = map(np.cumsum, rx_data)
 
     if axis:
-        sns.tsplot(data=data[0], condition='Without Reflexivity', ax=axis)
-        sns.tsplot(data=data[1], color='m', condition='With Reflexivity',
+        sns.tsplot(data=no_rx_data, condition='Without Reflexivity', ax=axis)
+        sns.tsplot(data=rx_data, color='m', condition='With Reflexivity',
                    ax=axis)
-        axis.set_title(r'$%s = %s$' % (par_name, str(par_value)),
-                       fontsize=fontsize)
+        axis.set_xlabel(r'$%s = %s$' % (par_name, str(par_value)),
+                        fontsize=fontsize)
         axis.legend(loc='upper right', fontsize=fontsize-2)
         axis.tick_params(axis='both', which='major', labelsize=fontsize-2)
     else:
-        sns.tsplot(data=data[0], condition='Without Reflexivity')
-        sns.tsplot(data=data[1], color='m', condition='With Reflexivity')
+        sns.tsplot(data=no_rx_data, condition='Without Reflexivity')
+        sns.tsplot(data=rx_data, color='m', condition='With Reflexivity')
         plt.title(r'$%s = %s$' % (par_name, str(par_value)), fontsize=fontsize)
         plt.legend(loc='upper right', fontsize=fontsize-2)
         plt.tick_params(axis='both', which='major', labelsize=fontsize-2)
