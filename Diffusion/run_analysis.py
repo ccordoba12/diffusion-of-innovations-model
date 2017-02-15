feb61f93e057f6d4efc5fe8475cb7da935b7d018
Carlos Cordoba <ccordoba12@gmail.com>
2017-02-15 15:16:56 -0500

    Diffusion: Move model parameters to its own all_parameters.py file

--- Diffusion/run_analysis.py
+++ Diffusion/run_analysis.py
@@ -16,42 +16,8 @@ from ipyparallel import Client
 
 from algorithm import compute_run, generate_parameters
 from plots import multiplot_adopters_and_global_utility
+from all_parameters import PARAMETERS_FILE
 from utils import LOCATION, load_parameters_from_file
-
-
-#==============================================================================
-# Parameters
-#==============================================================================
-# Parameters to run the analysis
-run = dict(
-    number_of_times = 70,
-    parameter_values = [0.4, 0.5, 0.6, 0.7],
-    cumulative = True,
-    main_parameter = 'quality',
-    max_time = 160
-)
-
-# Parameters for the model
-parameters = dict(
-    number_of_consumers = 1000,
-    social_influence = 0.7,
-    randomness = 0.01,
-    activation_sharpness = 40,
-    level = 1,
-    quality = 0.5,
-    initial_seed = 0.001,
-    critical_mass = 0.5,
-    number_of_neighbors = 8,
-    adopters_threshold = 0.5,
-    marketing_effort = 0,
-)
-
-# Load parameters from a file in SAVED_RESULTS_DIR.
-# Notes:
-# 1. This overrides the parameters set above
-# 2. Set this variable to '' to not load any
-#    file.
-PARAMETERS_FILE = 'social_influence_60.txt'
 
 
 #==============================================================================
@@ -73,6 +39,10 @@ if PARAMETERS_FILE:
         parameters = all_parameters['parameters']
     else:
         raise Exception('{} does not exist'.format(f))
+else:
+    # If there's no parameters file to load, used the parameters
+    # saved in all_parameters
+    from all_parameters import parameters, run
 
 
 #==============================================================================
