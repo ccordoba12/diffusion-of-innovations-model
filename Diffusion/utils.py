fd1eb178884da777319be06a7ac94a96f98362c0
Carlos Cordoba <ccordoba12@gmail.com>
2017-04-26 22:11:35 -0500

    Diffusion: Add global utility at a smaller value

--- Diffusion/utils.py
+++ Diffusion/utils.py
@@ -163,11 +163,11 @@ def compute_global_utility_activation_value(parameters):
     in the difussion process (but very slightly at the beginning
     though)
     """
-    for global_utility in np.arange(0, parameters['critical_mass'], 0.01):
+    for global_utility in np.arange(0, parameters['critical_mass'], 0.001):
         reflexivity_index = logistic(global_utility,
                                      parameters['activation_sharpness'],
                                      parameters['critical_mass'])
-        if reflexivity_index > 0.005:
+        if reflexivity_index > 1e-4:
             return global_utility
 
 
