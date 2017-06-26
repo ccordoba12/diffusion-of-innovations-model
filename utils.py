92e0887ee604ed320b0446dabc72fc35cc755941
Carlos Cordoba <ccordoba12@gmail.com>
2017-06-26 17:35:13 -0500

    Use a smaller value to in compute_global_utility_activation_value

--- utils.py
+++ utils.py
@@ -163,11 +163,11 @@ def compute_global_utility_activation_value(parameters):
     in the difussion process (but very slightly at the beginning
     though)
     """
-    for global_utility in np.arange(0, parameters['critical_mass'], 0.001):
+    for global_utility in np.arange(0, parameters['critical_mass'], 5e-6):
         reflexivity_index = logistic(global_utility,
                                      parameters['activation_sharpness'],
                                      parameters['critical_mass'])
-        if reflexivity_index > 1e-4:
+        if reflexivity_index > 5e-6:
             return global_utility
 
 
